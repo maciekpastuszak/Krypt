377da4509aaf069c9ee7330c5aab1302cd2ca01a
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-03-29 10:04:05 +0200

    bugfix - card layout

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -34,12 +34,13 @@ const Welcome = () => {
                         <div className={`rounded-br-2xl ${commonStyles}`}>Blockchain</div>
                     </div>
                 </div>
-                <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10"> 
+                <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
                     <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism ">
                         <div className="flex justify-between flex-col w-full h-full">
                             <div className="flex justify-between items-start">
                                 <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
                                     <SiEthereum fontSite={21} color="#fff" />
+                                </div>
                                     <BsInfoCircle fontSize={17} color="#fff" />
                                 </div>
                                 <div>
@@ -49,7 +50,7 @@ const Welcome = () => {
                                     <p className="text-white font-semibold text-lg mt-1">
                                         Ethereum
                                     </p>
-                                </div>
+                               
                             </div>
                         </div>
                     </div>
