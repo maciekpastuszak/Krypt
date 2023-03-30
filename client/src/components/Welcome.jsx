f5bc0751e4c04771e99a47357708bf46b039b82d
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-03-30 09:40:15 +0200

    add 'Send now' button

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -21,7 +21,11 @@ const Welcome = () => {
 
     const connectWallet = () => {
 
-    }
+    };
+
+    const handleSubmit = () => {
+
+    };
 
     return (
         <div className="flex w-full justify-center items-center">
@@ -69,6 +73,18 @@ const Welcome = () => {
                         <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={() => {}}/>
                         <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={() => {}}/>
                         <Input placeholder="Enter Message" name="message" type="text" handleChange={() => {}}/>
+                        <div className="h=[1px] w-full bg-gray-400 my-2" />
+                        {false ? (
+                            <Loader />
+                        ) : (
+                            <button
+                                type="button"
+                                onClick={handleSubmit}
+                                className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer"
+                            >
+                                Send Now
+                            </button>
+                        )}   
                     </div>
                 </div>
             </div>
