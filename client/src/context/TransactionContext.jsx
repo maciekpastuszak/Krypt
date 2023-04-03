d1750dd00a62aa91b215dc3449617993e9640024
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-03 10:09:07 +0200

    add wallet connecting

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -20,7 +20,7 @@ const getEthereumContract = () => {
 }
 
 export const TransactionProvider = ({ children }) => {
-    
+    const [connectedAccount, setconnectedAccount] = useState(initialState)
     const checkIfWalletIsConnected = async () => {
         if(!ethereum) return alert("Please install metamask");
 
@@ -29,12 +29,24 @@ export const TransactionProvider = ({ children }) => {
         console.log(accounts);
     }
 
+    const connectWallet = async () => {
+        try {
+            if(!ethereum) return alert("Please install metamask");
+
+            const accounts = await ethereum.request({ method: 'eth_requestAccounts'});
+            setCurrentAccount(accounts[0]);
+        } catch (error) {
+            console.log(error);
+            throw new Error("No ethereum object")
+        }
+    }
+
     useEffect(() => {
         checkIfWalletIsConnected();
     }, []);
-    
+
     return (
-        <TransactionContext.Provider value={{ value: 'test' }}>
+        <TransactionContext.Provider value={{ connectWallet }}>
             {children}
         </TransactionContext.Provider>
     )
