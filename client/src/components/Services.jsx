86629f5ff964ce3824ad51d348f464cbdc378309
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-11 22:04:22 +0200

    develop services component with wrapper styles

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -1,6 +1,17 @@
+import { BsShieldFillCheck } from "react-icons/bs";
+import { BiSearchAlt } from "react-icons/bi";
+import { RiHeart2Fill } from "react-icons/ri";
+
+
 const Services = () => {
     return (
-        <h1>Services</h1>
+        <div className="flex w-full justify-center items-center gradient-bg-services">
+            <div className="flex mf:flex-row flex-col items-center justify-between md:p-20 py-12 px-4">
+                <div className="flex-1 flex flex-col justify-start items-start">
+        
+                </div>
+            </div>
+        </div>
     );
 }
 
