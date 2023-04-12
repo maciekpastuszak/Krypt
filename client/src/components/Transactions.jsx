023e5540c5eca5b136f979d2aec158ff6b5014b9
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-12 10:06:15 +0200

    develop Transactions component

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,6 +1,16 @@
+import React, { useContext } from "react";
+
+import { TransactionContext } from "../context/TransactionContext";
+
+import dummyData from "../utils/dummyData";
+
 const Transactions = () => {
     return (
-        <h1>Navbar</h1>
+        <div className="flex w-full justofy-center 2xl:px-20 gradient-bg-transactions">
+            <div className="flex flex-col md:p-12 py-12 px-4">
+                {/* Latest transactions */}
+            </div>
+        </div>
     );
 }
 
