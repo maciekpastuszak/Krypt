159070fb2c83dd1ebf81a979cb1ec866f3ea1d87
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-12 22:26:37 +0200

    check if there are any transactions

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -5,10 +5,17 @@ import { TransactionContext } from "../context/TransactionContext";
 import dummyData from "../utils/dummyData";
 
 const Transactions = () => {
+
+    const { currentAccount } = useContext(TransactionContext);
     return (
         <div className="flex w-full justofy-center 2xl:px-20 gradient-bg-transactions">
             <div className="flex flex-col md:p-12 py-12 px-4">
-                {/* Latest transactions */}
+                {currentAccount ? (
+                    <h3 className="text-white text-3xl text-cener my-2">Latest transactions</h3>
+                ) : (
+                    <h3 className="text-white text-3xl text-cener my-2">
+                        Connect your account to see your latest transactions</h3>
+                )}
             </div>
         </div>
     );
