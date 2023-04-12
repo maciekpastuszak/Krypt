6f2ee87337e49216ca51e978412c3b0b9265834f
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-12 23:08:33 +0200

    create TransactionCard component with styled wrapper

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -7,6 +7,24 @@ import { shortenAddress } from "../utils/shortenAddress";
 
 const Transactions = () => {
 
+    const TransactionCard = ({addressTo, addressFrom, timestamp, message, keyword, amount, url}) => {
+        return (
+            <div className="bg-[#181918] m-4 flex flex-1
+                2xl:min-w-[450px]
+                2xl:max-w-[500px]
+                sm:min-w-[270px]
+                sm:max-w-[300px]
+                flex-col p-3 rounded-md hover:shadow-2xl
+            ">
+                <div className="flex flex-col items-center w-full mt-3">
+                    <div className="w-full mb-6 p-2">
+                       
+                    </div>
+                </div>
+            </div>
+        );
+    };
+
     const { currentAccount } = useContext(TransactionContext);
     return (
         <div className="flex w-full justofy-center 2xl:px-20 gradient-bg-transactions">
