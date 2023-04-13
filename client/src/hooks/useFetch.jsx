574b814d25c10d664811f51186dafc0535c8de27
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-13 22:34:41 +0200

    add useFetch hook

---
+++ client/src/hooks/useFetch.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+
+const API_KEY = import.meta.env.VITE_GIPHY_API;
+
+const useFetch = () => {
+    const [gifUrl, setGifUrl] = useState("");
+
+    const fetchGifs = async () => {
+        try {
+            const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${keyword.split(" ").join("")}&limit=1`);
+        } catch (error) {
+            
+        }
+    }
+}
