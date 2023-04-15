8983b2e9958d5d8b89e4e2b305a9b789e4557719
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-04-15 13:21:12 +0200

    fix fonts in footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -15,13 +15,13 @@ const Footer = () => {
                 </div>
             </div>
             <div className="flex justify-center items-center flex-col mt-5">
-                <p className="text-white text-sm text-center">Come join us</p>
-                <p className="text-white text-sm text-center">info@krypto.com</p>
+                <p className="text-white text-sm text-center">Come join us and hear for the unexpected miracle</p>
+                <p className="text-white text-sm text-center font-medium mt-2">info@kryptomastery.com</p>
             </div>
-            <div classsName="sm:w-[90%] w-full h-[0.25px] bg-grey-400 mt-5"></div>
+            <div className="sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5 " />
             <div className="sm:w-[90%] w-full flex justify-between items-center mt-3">
-                <p className="text-slate-50 text-sm text-center">@krypto 2023</p>
-                <p className="text-slate-50 text-sm text-center">All rights reserved</p>
+                <p className="text-gray-50 text-left text-xs">@kryptomastery2022</p>
+                <p className="text-gray-50 text-right text-xs">All rights reserved</p>
             </div>
         </div>
     );
